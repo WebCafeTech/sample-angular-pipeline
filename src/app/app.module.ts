d0a72ef86643f7e575092565cc701568dd7ba07e
Mukund Gohil <mukund.gohil@atos.net>
2022-10-31 02:55:52 +0530

    generated about and feature component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { SplashScreenComponent } from './splash-screen/splash-screen.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { AboutComponent } from './about/about.component';
+import { FeaturesComponent } from './features/features.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashScreenComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
